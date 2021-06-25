303b900bf76ae34c1f23e7a84c5911ff4c423b36
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-06-25 01:28:06 -0300

    Created User resource (methods ate not implemented)

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
