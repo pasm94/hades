2ae45b5aecc9411d63c00c438997a446412fd535
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-06-26 04:01:56 -0300

    feat(database): Database configs and connection created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { UsersModule } from './users/users.module';
+// import {} './database/migrations/'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'postgres',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      migrations: ['./database/migrations/*.ts'],
+      cli: {
+        migrationsDir: './database/migrations',
+      },
+    }),
+    UsersModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
