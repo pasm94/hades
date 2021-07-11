0090559649ce1ceb90863f9eb2a2aa316b244655
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-07-11 01:36:51 -0300

    fix(migration): fixed create tasks migartion

--- src/database/migrations/1625784598262-CreateTasks.ts
+++ src/database/migrations/1625784598262-CreateTasks.ts
@@ -1,89 +1,93 @@
-// import {
-//   MigrationInterface,
-//   QueryRunner,
-//   Table,
-//   TableForeignKey,
-// } from 'typeorm';
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
 
-// export class CreateTasks1625784598262 implements MigrationInterface {
-//   public async up(queryRunner: QueryRunner): Promise<void> {
-//     await queryRunner.createTable(
-//       new Table({
-//         name: 'tasks',
-//         columns: [
-//           {
-//             name: 'id',
-//             type: 'int',
-//             isPrimary: true,
-//             isGenerated: true,
-//             generationStrategy: 'increment',
-//           },
-//           {
-//             name: 'name',
-//             type: 'varchar',
-//           },
-//           {
-//             name: 'status',
-//             type: 'varchar',
-//             isUnique: true,
-//           },
-//           {
-//             name: 'prevision_date',
-//             type: 'date',
-//           },
-//           {
-//             name: 'started',
-//             type: 'timestamp',
-//           },
-//           {
-//             name: 'finished',
-//             type: 'timestamp',
-//           },
-//           {
-//             name: 'created_at',
-//             type: 'timestamp',
-//             default: 'now()',
-//           },
-//           {
-//             name: 'user_id',
-//             type: 'int',
-//           },
-//           {
-//             name: 'project_id',
-//             type: 'int',
-//           },
-//         ],
-//       }),
-//     );
+export class CreateTasks1625784598262 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tasks',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+          },
+          {
+            name: 'prevision_date',
+            type: 'date',
+            isNullable: true,
+          },
+          {
+            name: 'started',
+            type: 'timestamp',
+            isNullable: true,
+          },
+          {
+            name: 'finished',
+            type: 'timestamp',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'user_id',
+            type: 'int',
+            isNullable: true,
+          },
+          {
+            name: 'project_id',
+            type: 'int',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
 
-//     await queryRunner.createForeignKey(
-//       'tasks',
-//       new TableForeignKey({
-//         name: 'TaskUser',
-//         columnNames: ['user_id'],
-//         referencedColumnNames: ['id'],
-//         referencedTableName: 'users',
-//         onDelete: 'SET NULL',
-//         onUpdate: 'CASCADE',
-//       }),
-//     );
+    await queryRunner.createForeignKey(
+      'tasks',
+      new TableForeignKey({
+        name: 'TaskUser',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
 
-//     await queryRunner.createForeignKey(
-//       'tasks',
-//       new TableForeignKey({
-//         name: 'TaskProject',
-//         columnNames: ['project_id'],
-//         referencedColumnNames: ['id'],
-//         referencedTableName: 'projects',
-//         onDelete: 'SET NULL',
-//         onUpdate: 'CASCADE',
-//       }),
-//     );
-//   }
+    await queryRunner.createForeignKey(
+      'tasks',
+      new TableForeignKey({
+        name: 'TaskProject',
+        columnNames: ['project_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'projects',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
 
-//   public async down(queryRunner: QueryRunner): Promise<void> {
-//     await queryRunner.dropForeignKey('tasks', 'TaskUser');
-//     await queryRunner.dropForeignKey('tasks', 'TaskProject');
-//     await queryRunner.dropTable('tasks');
-//   }
-// }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('tasks', 'TaskProject');
+    await queryRunner.dropForeignKey('tasks', 'TaskUser');
+    await queryRunner.dropTable('tasks');
+  }
+}
---
+++ todos.md
@@ -0,0 +1,1 @@
+ver create connection em ormconfig
